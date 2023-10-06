d4b94a4ab5a9741ae8b8d09155ede0aab906a15b
Anukul Karki <anukulkarki11@gmail.com>
2023-10-06 16:30:51 +0545

    crudStudent

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import crud, classroomDelete, classroomUpdate, student, addStudent
+from .views import crud, classroomDelete, classroomUpdate, student, addStudent,studentDetail, studentUpdate
 
 urlpatterns = [
     path("",crud, name="crudClassroom"),
     path("class/delete/<int:id>/",classroomDelete, name="classroomDelete"),
     path("class/update/<int:id>/",classroomUpdate, name="classroomUpdate"),
     path("student/", student, name="crudStudents"),
-    path("addStudent/",addStudent, name="addStudent")
+    path("addStudent/",addStudent, name="addStudent"),
+    path("student/detail/<int:id>/",studentDetail, name="studentDetail"),
+    path("student/update/<int:id>",studentUpdate, name="studentUpdate")
 ]
--- crud/views.py
+++ crud/views.py
@@ -32,16 +32,17 @@ def student(request:HttpRequest):
     students = Student.objects.all()
     return render(request, "crud/student.html", {"students":students, "title":"Student"})
 
-def addStudent(request:HttpRequest):
+def addStudent(request:HttpRequest): 
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
         age = request.POST.get("age")
         address = request.POST.get("address")
         contact = request.POST.get("contact")
+        classroomName = request.POST.get("classroomId")
         pp = request.FILES.get('pp')
 
-        StudentObject = Student.objects.create(name=name, email=email, age=age, classroom_id = 1)
+        StudentObject = Student.objects.create(name=name, email=email, age=age, classroom_id = classroomName)
         StudentProfileObject = StudentProfile.objects.create(contact=contact, address= address, student = StudentObject)
         if pp:
             StudentProfileObject.picture = pp
@@ -49,6 +50,31 @@ def addStudent(request:HttpRequest):
 
         return redirect("crudStudents")
 
-    return render(request, "crud/addStudent.html", {"title":"Add Student"})
+    return render(request, "crud/addStudent.html", {"title":"Add Student", "class": Classroom.objects.all() })
+
+def studentDetail(request:HttpRequest, id):
+    StudentObject = Student.objects.get(id=id)
+    return render(request, "crud/studentDetail.html", {"title":"Student Details", "student":StudentObject})
+
+def studentUpdate(request:HttpRequest, id):
+    StudentObject = Student.objects.get(id=id)
+
+    if request.method == "POST":
+        updatedName = request.POST.get('name')
+        updatedEmail = request.POST.get('email')
+        updatedAge = request.POST.get('age')
+        updatedContact = request.POST.get('contact')
+        updatedAddress = request.POST.get('address')
+        updatedPhoto = request.FILES.get('pp')
+
+        Student.objects.filter(id=id).update(name=updatedName, email = updatedEmail, age = updatedAge)
+
+        sp, b = StudentProfile.objects.update_or_create(student = StudentObject, defaults={"address":updatedAddress, "contact":updatedContact}) #Return Tuple (object StudentProfile Object, boolean (True for Create, False for update))
+
+        if updatedPhoto:
+            sp.picture = updatedPhoto
+            sp.save()
+        return redirect("studentDetail", StudentObject.id)
+    return render(request, "crud/studentUpdate.html", {"title":"Student Update", "student":StudentObject})
 
 
