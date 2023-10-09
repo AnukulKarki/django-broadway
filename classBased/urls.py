80f1d8fd5b049f409a6409bc1f404ca57c0f88ad
Anukul Karki <anukulkarki11@gmail.com>
2023-10-09 17:17:49 +0545

    Template View

--- classBased/urls.py
+++ classBased/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import classroom, model_classroom
+from .views import classroom, model_classroom, classroomView, classroomTemplateView, ClassroomCreateView, StudentListView
 
 urlpatterns = [
     path("classroom/", classroom, name="cb_classroom"),
     path("model-classroom/", model_classroom, name="cb_model_classroom"),
+    path("simple-classroom/",classroomView.as_view()), #as_view --> call the method according to the method type.
+    path("template-classroom/",classroomTemplateView.as_view()),
+    path("create-classroom",ClassroomCreateView.as_view()),
+    path("student-list/", StudentListView.as_view()),
+    
 ]
--- classBased/views.py
+++ classBased/views.py
@@ -1,7 +1,11 @@
+from typing import Any
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from crud.models import *
 from .forms import ClassroomForm, ClassroomModelForm
 from django.http import HttpRequest
+from django.views import View
+from django.views.generic import TemplateView, CreateView, ListView
 
 # Create your views here.
 def classroom (request:HttpRequest):
@@ -29,3 +33,47 @@ def model_classroom(request:HttpRequest):
     form = ClassroomModelForm()
     classrooms = Classroom.objects.all()
     return render(request,"classBased/classroom.html", {"title":"classroom","classrooms":classrooms, "form":form})
+
+
+class classroomView(View):
+
+    def get(self, *args, **kwargs):
+        classroom = Classroom.objects.all()
+        form = ClassroomModelForm()
+        return render(self.request,"classbased/classroom.html", {"classrooms":classroom, "form":form})
+
+    def post(self, *args, **kwargs):
+        form = ClassroomModelForm(self.request.POST)
+        if form.is_valid():
+            form.save()  #can only be used in model form
+        return redirect("cb_classroom")
+    
+class classroomTemplateView(TemplateView):
+    template_name = "classBased/classroom.html"
+
+    def get_context_data(self, **kwargs ):
+        context = {"classrooms":Classroom.objects.all(), "form":ClassroomModelForm()}
+        return context
+    
+class ClassroomCreateView(CreateView):
+    form_class = ClassroomModelForm
+    queryset = Classroom.objects.all()
+    template_name = "classbased/classroom.html"
+    success_url = reverse_lazy("cb_classroom")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["classrooms"] = self.queryset
+        return context
+    
+class StudentListView(ListView):
+    queryset = Student.objects.all()
+    template_name = "classBased/student.html"
+    context_object_name = "students" #change the key of the dictionary which takes the data from query set
+
+    
+    # def get_context_data(self,* args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     # print(context)
+    #     # context["students"] = self.queryset
+    #     return context
